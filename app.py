56b8f554ac84b52d9d7460104de492fea43af906
Gabriel <gabriel@debian>
2023-01-04 00:35:45 -0300

    Mad Libs Game.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-num1 = input("Enter first number: ")
-num2 = input("Now, the second: ")
-result = float(num1) + float(num2)
+color = input("Enter a color: ")
+plural_noun = input("Now, a Plural Noun: ")
+food = input("Finally, a food: ")
 
-print("The sum of those two numbers is:\n" + str(result))
+print("Roses are " + color + "\n"+ 
+    plural_noun + " are blue\n" +
+    "I love " + food
+)
