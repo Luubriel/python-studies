53ed00a9c809fd0030bf32a60d937f92f709cb7f
Gabriel <gabriel@debian>
2023-01-04 00:53:18 -0300

    Lists.

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
-color = input("Enter a color: ")
-plural_noun = input("Now, a Plural Noun: ")
-food = input("Finally, a food: ")
+games = ["Dark Souls Franchise", "The Elder Scrolls V: Skyrim", 
+"Half-Life Franchise", "Papers, Please"]
 
-print("Roses are " + color + "\n"+ 
-    plural_noun + " are blue\n" +
-    "I love " + food
-)
+print(games[-1])
+
+print(games[2:4])
+
+some_numbers = [5, 1, 52, 74, 55, 9]
+
+games.extend(some_numbers)
+
+print(games)
+
+# add an item to the list
+games.append("Infectonator")
+
+# add a item to a specific index on the list, without remove the item on index
+games.insert(1, "Welcome to the game")
+games.insert(4, "Welcome to the game")
+
+some_numbers.sort()
+
+games.reverse()
+
+print(str(games) + "\n" + str(some_numbers))
+
+games2 = games.copy()
+
+
+games.remove(1)
+
+# pop remove the last item of the list
+games.pop()
+
+games.clear()
+
