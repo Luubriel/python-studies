3ac10846a26d3de80e9c03e701d97c19071b1bf3
Gabriel <gabriel@debian>
2023-01-23 22:34:14 -0300

    Functions, return statements, if statements, comparisons and a taste of lambda.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Python Studies
 
 Python studies based on [Learn Python - Full Course for Beginners [Tutorial]](https://yewtu.be/watch?v=rfscVS0vtbw).  
-
+(And some internet searchs)  
 ![Python Two Snakes](assets/python-logo-only.png)
--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-# This is a tuple
-# when assigned value, they cannot be changed
-coordinates = (4, 5)
+from datetime import datetime
+import os
 
-# This is a list
-fruits = ['orange', 'apple', 'banana']
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
 
-print(coordinates[1])
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
 
-print(fruits)
+## lambda is used to create anonymous functions
+# clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+
+def greet(name):
+    clear()
+    if current_time > '05:00:00' and current_time < '11:59:59':
+        print("Good morning " + name + "!")
+    elif current_time > '12:00:00' and current_time < '16:59:59':
+        print("Good afternoon " + name + "!")
+    else:
+        print("Good evening " + name + "!")
+
+def cube(num):
+    result = num*num*num
+    return result
+
+name = input('Say your name: ')
+age = int(input('Now your age: '))
+
+cubed_age = cube(age)
+    
+greet(name)
+print('Your age cubed is: ' + str(cubed_age))
