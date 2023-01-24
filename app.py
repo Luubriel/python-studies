7cfb7bc163047a12f073391d3816e1e7815cfe87
Gabriel <gabriel@debian>
2023-01-23 21:43:03 -0300

    Tuples and lists.

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
-games = ["Dark Souls Franchise", "The Elder Scrolls V: Skyrim", 
-"Half-Life Franchise", "Papers, Please"]
+# This is a tuple
+# when assigned value, they cannot be changed
+coordinates = (4, 5)
 
-print(games[-1])
+# This is a list
+fruits = ['orange', 'apple', 'banana']
 
-print(games[2:4])
+print(coordinates[1])
 
-some_numbers = [5, 1, 52, 74, 55, 9]
-
-games.extend(some_numbers)
-
-print(games)
-
-# add an item to the list
-games.append("Infectonator")
-
-# add a item to a specific index on the list, without remove the item on index
-games.insert(1, "Welcome to the game")
-games.insert(4, "Welcome to the game")
-
-some_numbers.sort()
-
-games.reverse()
-
-print(str(games) + "\n" + str(some_numbers))
-
-games2 = games.copy()
-
-
-games.remove(1)
-
-# pop remove the last item of the list
-games.pop()
-
-games.clear()
-
+print(fruits)
