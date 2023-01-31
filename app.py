598a8ec314054d0fbacb9d153098cad6425781f5
Gabriel <gabriel@debian>
2023-01-30 23:04:34 -0300

    Dictonaries.

--- app.py
+++ app.py
@@ -1,32 +1,20 @@
-from datetime import datetime
-import os
 
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"    
+}
 
-def clear():
-    os.system('cls' if os.name=='nt' else 'clear')
+print(monthConversions["Mar"])
 
-## lambda is used to create anonymous functions
-# clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
-
-def greet(name):
-    clear()
-    if current_time > '05:00:00' and current_time < '11:59:59':
-        print("Good morning " + name + "!")
-    elif current_time > '12:00:00' and current_time < '16:59:59':
-        print("Good afternoon " + name + "!")
-    else:
-        print("Good evening " + name + "!")
-
-def cube(num):
-    result = num*num*num
-    return result
-
-name = input('Say your name: ')
-age = int(input('Now your age: '))
-
-cubed_age = cube(age)
-    
-greet(name)
-print('Your age cubed is: ' + str(cubed_age))
+# get function return a default answer in case the key is not found in dictionary
+print(monthConversions.get("Lol", "Not a valid key"))
